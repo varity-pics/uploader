00270a27f9f9d131b15f1c76797f8e315c2cda8f
wikomiks <97792998+wikomiks@users.noreply.github.com>
2022-10-06 22:54:32 +0200

    code cleanup i cos jeszcze

--- src/index.js
+++ src/index.js
@@ -8,21 +8,18 @@ const fs = require("fs");
 const express = require("express");
 const prisma = new db.PrismaClient();
 const port = 80;
-const strlength = 5;
 const mysql = require("mysql2")
 var connection = mysql.createConnection({ host: "localhost", user: "root", password: "magicznysiurek213769420.hostlol", database: "69420"});
 const app = express();
 app.use(express.json());
 
-const chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz23456789";
+const chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
 
 app.listen(port, () => {
   console.log(`Uploader started on port ${port}`);
 });
 
-// uploader part
 app.post("/", upload.single("69420"), async (req, res) => {
-  // wypisuje nazwe oraz typ pliku
   try {
     const file = req.file
     //console.log(file.originalname, file.mimetype);
@@ -32,24 +29,18 @@ app.post("/", upload.single("69420"), async (req, res) => {
         token: token,
       },
     });
-    ////////res.send(tokenExist);
-    //console.log(data);
     if (!data) {
       res.status(401).json({ error: "Your upload token invalid." });
       fsExtra.emptyDirSync(__dirname+"/temp");
       return;
     }
-    //console.log("dupa")
     const banned = false;
 
     if (banned) {
-      //sprawdzanie czy uzytkownik jest zbanowany
       res.status(403);
       fsExtra.emptyDirSync(__dirname+"/temp");
       return res.json({ error: `You are blacklisted for: <reason>` });
     }
-
-    //console.log(data); // i to juz jest prased json
     const domain = "cool.wikomiks.space";
 
     const fileNameMethod = data.fileNameMethod;
@@ -60,10 +51,6 @@ app.post("/", upload.single("69420"), async (req, res) => {
     const embedSiteName = data.embedSiteName;
     const embedTitle = data.embedTitle;
  
-
-    //if (fileNameMethod == ("normal"))
-
-    // sprawdzanie jakiego typu jest plik
     if (
       req.file.mimetype == "image/png" ||
       req.file.mimetype == "image/jpeg" ||
@@ -77,25 +64,20 @@ app.post("/", upload.single("69420"), async (req, res) => {
     } else {
       return res.status(415).json({ error: "Not supported media type" }); // gdy plik nie jest wspierany wywala error
     }
-    // u+200b i u+200c
-    // generowanie randomowego ciagu znakow
 
-    
-    //console.log(fileNameMethod);
     if (fileNameMethod === "normal") {
       var realFileName = randomString()+fileType;
       var Alias = realFileName
       var url = ("https://" + domain + "/" + Alias)
     }
+
     else if (fileNameMethod === "invisible") {
 
       const invisibleChars = "01";
-      //let invisibleFileName = invisibleArray.sort(() => .5 - Math.random()).slice(0,n)
       const randomArray = Array.from(
         { length: 20 },
         () => invisibleChars[Math.floor(Math.random() * invisibleChars.length)]);
       let stringMain = randomArray.join("");
-      //unicodes and other shit
       var string = ""
       for (var i = 0; i < stringMain.length; i++) {
         if (stringMain.charAt(i) == "0") { string=string+"​"}
@@ -106,8 +88,7 @@ app.post("/", upload.single("69420"), async (req, res) => {
       var realFileName = (realFileName+fileType)
       var url = ("https://" + domain + "/" + string)
     }
-
-
+    
     else if (fileNameMethod === "emoji") {
       var realFileName = randomString();
       var realFileName = (realFileName+fileType)
@@ -115,12 +96,9 @@ app.post("/", upload.single("69420"), async (req, res) => {
       var url = ("https://" + domain + "/" + Alias)
     }
     
-
-    // przeniesienie pliku z /temp do /content folder
     const oldPath = path.join(__dirname + "/temp/" + file.filename);
     const newPath = path.join(__dirname + "/contentfolder/" + realFileName);
     fs.rename(oldPath, newPath, () => {});
-    // tutaj
     if (fileNameMethod === "normal" || fileNameMethod === "invisible"){
       await prisma.files.create({
         data: 
@@ -135,10 +113,11 @@ app.post("/", upload.single("69420"), async (req, res) => {
           embedDesc: embedDesc,
           embedColor: embedColor,
           uploadDate: (Date.now().toString()),
+          Mimetype: fileType,
         },
       });
-    } else {
-      
+    } else { 
+
     }
     return res.status(201).json({ url: url });
   } catch(err) {
@@ -148,18 +127,12 @@ app.post("/", upload.single("69420"), async (req, res) => {
 // real filenames
 function randomString() {
   const randomArray = Array.from(
-    { length: strlength },
+    { length: 8 },
     () => chars[Math.floor(Math.random() * chars.length)]
   );
   let realFileName = randomArray.join("");
   return realFileName;
 }
-
-
-// invisible filenames
-
-
-
 // emoji filenames
 function emojiString() {
   const emojiChars = "seks";
@@ -179,13 +152,9 @@ app.get("/:params", async (req, res) => {
     if (content.startsWith("​") || content.startsWith("‌")) {
       var string2 = ""
       for (var i = 0; i < content.length; i++) {
-        
         if (content.charAt(i) == "​") { string2=string2+"0"} // u+200b
         else if (content.charAt(i) == "‌") { string2=string2+"1"} // u+200c
-        //console.log(content.charAt(i))
-        //content = string2
       }
-      //console.log(string2)
       content = string2
     }
 
@@ -196,70 +165,106 @@ app.get("/:params", async (req, res) => {
       }
     });
     if (!data) {return res.send("file not found")}
+    if (data.Mimetype === ".png" || data.Mimetype === ".gif") {
+      videoOrPic = `<meta property="og:image" content="/content/${(data.fileName)}">
+      <meta property="twitter:card" content="summary_large_image">` 
+    }
+    else {videoOrPicbody = `<video width="400" controls> <source src=${(data.fileName)} type="video/mp4"></video>`}
       res.send(`
-      <head>
-          <meta name="theme-color" content="${data.embedColor}">
-          <meta property="og:image" content="/content/${(data.fileName)}">
-          <meta property="og:title" content="${data.embedTitle}">
-          <meta property="og:description" content="${data.embedDesc}">
-          <meta property="og:url" content="https://69420.host">
-          <meta property="og:site_name" content="${data.embedSiteName}">
-          <meta property="og:type" content="website">
-          <meta property="twitter:card" content="summary_large_image">
-      </head>
-      <body>
-        <center>
-          <div>
-            <p>${(data.fileName)} and many other files are hosted on 69420.host</p>
-            <img src=/content/${(data.fileName)}>
-          </div>
-        </cetner>
-      </body>
-      <style>
-      @keyframes animatebackground {
-        from {
-        background-position-y: 0;
-        background-position-x: 0;
-      }
-      to {
-        background-position-y: 200rem;
-        background-position-x: 200rem;
-      }
-      }
-      body {
-        background-color: black!important;
-        background-image: url('https://i.imgur.com/fgbHD5v.png')!important;
-        background-size: 30rem!important;
-        color: white!important;
-        font-family: proxima-soft, sans-serif!important;
-        overflow-x: hidden!important;
-        margin: 0!important;
-        animation: animatebackground 100s infinite linear!important;
-      }
-      img {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        -webkit-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-        max-width: 65vw;
-        max-height: 65vh;
-        }
-      </style> 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta name="theme-color" content="${data.embedColor}" />
+    <meta property="og:image" content="/content/${data.fileName}" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="og:title" content="${data.embedTitle}" />
+    <meta property="og:description" content="${data.embedDesc}" />
+    <meta property="og:url" content="https://69420.host" />
+    <meta property="og:site_name" content="${data.embedSiteName}" />
+    <meta property="og:type" content="website" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap"
+      rel="stylesheet"
+    />
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="content">
+      <div style="position: absolute; top: 0; margin-top: 5px; font-size: 20px">
+        <p>${data.fileName}</p>
+      </div>
+      <img class="image" src="/content/${data.fileName}" />
+      <div>
+        <h3>Uploaded by: ${data.owner}</h3>
+        <h3>Uploaded on: ${data.uploadDate}</h3>
+      </div>
+      <button class="button">Report Image</button>
+    </div>
+  </body>
+
+  <style>
+    body {
+      display: flex;
+      justify-content: center;
+      box-sizing: border-box;
+      font-family: "Quicksand", sans-serif;
+      text-align: center;
+      background-color: #121212;
+      color: white;
+    }
+
+    .content {
+      display: flex;
+      gap: 5vh;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      background-color: #161616;
+      border-width: 1px;
+      border-style: solid;
+      border-color: rgb(38 38 38);
+      border-radius: 10px;
+      height: 65vh;
+      width: 60vw;
+      margin-top: 15px;
+    }
+
+    .image {
+      vertical-align: middle;
+      border-radius: 3.5px;
+      max-height: 30vh;
+      width: auto;
+      max-width: 100%;
+      margin-top: 10px;
+    }
+
+    .button {
+      display: block;
+      color: white;
+      background: rgb(185 28 28);
+      width: 200px;
+      height: 30px;
+      border-radius: 10px;
+      border-color: transparent;
+    }
+    .button:hover {
+      cursor: pointer;
+      background: rgb(153 27 27);
+    }
+  </style>
+</html>
       `)
   }catch {
     return res.send("error")
   }
 })
-
-
-
-
-
-
-
-
-
 
 
 app.get("/test", async (req, res) => {
@@ -269,7 +274,7 @@ app.get("/test", async (req, res) => {
 
 app.use("/content", express.static(__dirname + "/contentfolder"));
 app.use(function (req, res, next) {
-  res.removeHeader("X-Powered-By"); // usuwa xpoweredby express
+  res.removeHeader("X-Powered-By");
   next();
 });
 
